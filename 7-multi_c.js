196b2b74459fa640c6c108f81d71194529bc3184
Neaman <197640394+nmncode9@users.noreply.github.com>
2025-05-20 19:18:34 +0100

    Create 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,6 @@
+if (process.argv[2]) {
+  if (process.argv[2] > 0) {
+    for (let i=0;i<process.argv[2].length;i++) console.log("C is fun")
+  }
+} else console.log("Missing number of occurrences")
+
